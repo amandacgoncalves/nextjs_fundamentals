e784708f93bd2574dd94acb65a97a832b744bd72
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-31 12:36:14 -0300

    navigator component

--- src/pages/jsx.jsx
+++ src/pages/jsx.jsx
@@ -1,13 +1,17 @@
+import Layout from "../components/Layout"
+
 export default function Jsx() {
     const titulo = <h1>JSX é um conceito central</h1>
     function subtitulo() {
         return <h2>{"muito legal".toUpperCase()}</h2>
     }
     return (
+        <Layout titulo="Entendendo jsx">
         <div>
             {titulo}
             {subtitulo()}
             <p>{JSON.stringify({nome: 'Amanda', idade: 19})}</p>
         </div>
+        </Layout>
     )
 }
