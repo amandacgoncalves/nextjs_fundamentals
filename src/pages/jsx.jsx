3a382bcbe09d0853d295fce71cbf08d6cea04b51
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-31 11:00:33 -0300

    jsx/javascript

---
+++ src/pages/jsx.jsx
@@ -0,0 +1,13 @@
+export default function Jsx() {
+    const titulo = <h1>JSX é um conceito central</h1>
+    function subtitulo() {
+        return <h2>{"muito legal".toUpperCase()}</h2>
+    }
+    return (
+        <div>
+            {titulo}
+            {subtitulo()}
+            <p>{JSON.stringify({nome: 'Amanda', idade: 19})}</p>
+        </div>
+    )
+}
