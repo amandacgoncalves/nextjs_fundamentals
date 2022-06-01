1d94cb35079b30f839ca15b72ecf4884916cdf52
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-06-01 10:38:49 -0300

    pre render

--- src/pages/integracao_1.jsx
+++ src/pages/integracao_1.jsx
@@ -7,13 +7,17 @@ export default function Integracao() {
   function obterCliente() {
     fetch(`http://localhost:3000/api/clientes/${codigo}`)
       .then((resp) => resp.json())
-      .then((dados) => setCliente(dados))
+      .then((dados) => setCliente(dados));
   }
 
   return (
     <Layout>
       <div>
-        <input type="number" value={codigo} onChange={e => setCodigo(e.target.value)}/>
+        <input
+          type="number"
+          value={codigo}
+          onChange={(e) => setCodigo(e.target.value)}
+        />
         <button onClick={obterCliente}>Obter Cliente</button>
       </div>
       <ul>
